d0e3fc807ffca9fc060df1f4c7bafd3fee351508
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-06 14:52:27 -0300

    funcionalidade básica ok

--- diamantes.rb
+++ diamantes.rb
@@ -1,1 +1,21 @@
-<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>
+express = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
+
+#array_express = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>".split('')
+#array_sand = array_express.select { |sand| sand == "." }
+#array_sand.length
+#array_diamond = array_express.select { |diamond| diamond == "<" || diamond == ">" }
+#count_diamond = array_diamond.length / 2
+
+def count_sand(express)
+  array_express = express.split('')
+  array_sand = array_express.select { |sand| sand == "." }
+  return array_sand.length
+end
+
+def count_diamond(express)
+  array_express = express.split('')
+  array_diamond = array_express.select { |diamond| diamond == "<" || diamond == ">" }
+  return count_diamond = array_diamond.length / 2
+end
+
+puts count_diamond(express), count_sand(express)
