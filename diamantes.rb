b97c6e4c62b8065276b23ef7049e79cdffe3ec6c
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-06 22:17:11 -0300

    interface-ok-1

--- diamantes.rb
+++ diamantes.rb
@@ -1,18 +1,5 @@
-# def count_sand(express)
-#   array_express = express.split('')
-#   array_sand = array_express.select { |sand| sand == "." }
-#   return array_sand.length
-# end
-
-# def count_diamond(express)
-#   array_express = express.split('')
-#   array_diamond = array_express.select { |diamond| diamond == "<" || diamond == ">" }
-#   return count_diamond = array_diamond.length / 2
-# end
-
-#puts count_diamond(express), count_sand(express)
-
 exp = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
+
 def extracao_de_diamantes(exp)
   # puts exp
   exp.gsub!('.', '')
